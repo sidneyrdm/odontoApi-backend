0f004fdbf5a1a9125ace454130b90db8c4da3b3f
sidneyrdm <sidneybritomal@gmail.com>
2021-10-13 10:33:39 -0300

    teste para primeiro deploy

---
+++ src/main/java/io/github/sidneyrdm/clientes/model/repository/ServicoPrestadoRepository.java
@@ -0,0 +1,22 @@
+package io.github.sidneyrdm.clientes.model.repository;
+
+import io.github.sidneyrdm.clientes.model.entity.ServicoPrestado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ServicoPrestadoRepository extends JpaRepository<ServicoPrestado, Integer> {
+
+
+    @Query(" select s from ServicoPrestado s join s.cliente c " +
+            " where upper( c.nome ) like upper( :nome ) and MONTH(s.data) =:mes    ")
+    List<ServicoPrestado> findByNomeClienteAndMes(
+            @Param("nome") String nome, @Param("mes") Integer mes);
+
+    @Query(" select s from ServicoPrestado s join s.cliente c " +
+            " where upper( c.nome ) like upper( :nome )")
+    List<ServicoPrestado> findByNomeCliente(
+            @Param("nome") String nome);
+}
---
+++ src/main/java/io/github/sidneyrdm/clientes/rest/ServicoPrestadoController.java
@@ -0,0 +1,65 @@
+package io.github.sidneyrdm.clientes.rest;
+
+import io.github.sidneyrdm.clientes.model.entity.Cliente;
+import io.github.sidneyrdm.clientes.model.entity.ServicoPrestado;
+import io.github.sidneyrdm.clientes.model.repository.ClienteRepository;
+import io.github.sidneyrdm.clientes.model.repository.ServicoPrestadoRepository;
+import io.github.sidneyrdm.clientes.rest.dto.ServicoPrestadoDTO;
+import io.github.sidneyrdm.clientes.util.BigDecimalConverter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/servicos-prestados")
+@RequiredArgsConstructor
+public class ServicoPrestadoController  {
+
+    private final ClienteRepository clienteRepository;
+    private final ServicoPrestadoRepository repository;
+    private final BigDecimalConverter bigDecimalConverter;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ServicoPrestado salvar(@RequestBody @Valid ServicoPrestadoDTO dto ){
+        LocalDate data = LocalDate.parse(dto.getData(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        Integer idCliente = dto.getIdCliente();
+
+        Cliente cliente =
+                clienteRepository
+                        .findById(idCliente)
+                        .orElseThrow(() ->
+                                new ResponseStatusException(
+                                        HttpStatus.BAD_REQUEST, "Cliente inexistente."));
+
+
+        ServicoPrestado servicoPrestado = new ServicoPrestado();
+        servicoPrestado.setDescricao(dto.getDescricao());
+        servicoPrestado.setData( data );
+        servicoPrestado.setCliente(cliente);
+        servicoPrestado.setValor( bigDecimalConverter.converter(dto.getPreco())  );
+
+        return repository.save(servicoPrestado);
+    }
+
+    @GetMapping
+    public List<ServicoPrestado> pesquisar(
+            @RequestParam(value = "nome", required = false, defaultValue = "") String nome,
+            @RequestParam(value = "mes", required = false) Integer mes
+    ) {
+        return repository.findByNomeClienteAndMes("%" + nome + "%", mes);
+    }
+
+    @GetMapping("/consulta")
+    public List<ServicoPrestado> pesquisar(
+            @RequestParam(value = "nome", required = false, defaultValue = "") String nome
+    ) {
+        return repository.findByNomeCliente("%" + nome);
+    }
+}
